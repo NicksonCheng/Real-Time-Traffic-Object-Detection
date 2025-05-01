d66d989b6a7747e637ad28c6a25b0f4695e6978e
Nickson <zz20102aa@gmail.com>
2025-05-01 16:11:51 +0000

    preprocess dataset to yolo input format

---
+++ download.py
@@ -0,0 +1,11 @@
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("solesensei/solesensei_bdd100k")
+
+# print("Path to dataset files:", path)
+
+
+import dataset_tools as dtools
+
+dtools.download(dataset='BDD100K: Images 100K', dst_dir='dataset-ninja')
